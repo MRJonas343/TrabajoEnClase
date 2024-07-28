c53f8b1aca3a1e1824b0a54fc2be38ce273ad9af
MrJonas343 <angeljonasrosales@gmail.com>
2024-07-28 14:09:08 -0500

    chore: Refactor ScreenSetting component
    Refactor the ScreenSetting component in the Setting directory. Clean up the code by removing unnecessary imports and formatting the code according to the established style guide.

--- src/componentes/screen/Setting/ScreenSetting.js
+++ src/componentes/screen/Setting/ScreenSetting.js
@@ -1,32 +1,37 @@
-import { StyleSheet, Text, View,Alert } from 'react-native'
-import React from 'react'
-import { Button } from 'react-native-paper';
-import { useNavigation } from '@react-navigation/native';
-import { getAuth, signOut } from "firebase/auth"
+import { StyleSheet, Text, View, Alert } from "react-native";
+import React from "react";
+import { Button } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import { getAuth, signOut } from "firebase/auth";
 
 const ScreenSetting = () => {
-  const navegacion = useNavigation()
-  const cerrarsesion =()=>{
-    const auth = getAuth();
-    signOut(auth)
-      .then(() => {
-        Alert.alert('Aviso','Sesión cerrada correctamente')
-        navegacion.navigate('Login');
-      })
-      .catch((error) => {
-        console.error('Error cerrando la sesión: ', error);
-      });
-  }
-  return (
-    <View>
-      <Text>Autor: Carlos Antonio Balcazar Olan</Text>
-      <Button style={{marginTop:20}} icon="logout" mode="contained" onPress={() => cerrarsesion()}>
-        Cerrar Seccion
-      </Button>
-    </View>
-  )
-}
+	const navegacion = useNavigation();
+	const cerrarsesion = () => {
+		const auth = getAuth();
+		signOut(auth)
+			.then(() => {
+				Alert.alert("Aviso", "Sesión cerrada correctamente");
+				navegacion.navigate("Login");
+			})
+			.catch((error) => {
+				console.error("Error cerrando la sesión: ", error);
+			});
+	};
+	return (
+		<View>
+			<Text>Angel Jonas Rosales Gonzlez</Text>
+			<Button
+				style={{ marginTop: 20 }}
+				icon="logout"
+				mode="contained"
+				onPress={() => cerrarsesion()}
+			>
+				Cerrar Seccion
+			</Button>
+		</View>
+	);
+};
 
-export default ScreenSetting
+export default ScreenSetting;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
